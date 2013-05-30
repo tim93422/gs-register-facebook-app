e6fae12fd00f17d64e2dc4ebcbd22eb6d6962abb
Greg Turnquist <gturnquist@gopivotal.com>
2013-05-30 13:52:26 -0400

    Replace tabs with spaces in the code.
    In accordance with guidelines, edited Main to replace it's tabs with spaces.

--- src/main/java/fbclient/Main.java
+++ src/main/java/fbclient/Main.java
@@ -10,72 +10,63 @@ import org.springframework.social.oauth2.OAuth2Operations;
 
 public class Main {
 
-	public static void main(String[] args) {
-		String appId = promptForInput("Enter your App ID:");
-		String appSecret = promptForInput("Enter your App Secret:");
-		String appToken = fetchApplicationAccessToken(appId, appSecret);
-		AppDetails appDetails = fetchApplicationData(appId, appToken);
-		System.out.println("\n   APPLICATION DETAILS");
-		System.out.println("=========================");
-		System.out.println("ID:             " + appDetails.getId());
-		System.out.println("Name:           " + appDetails.getName());
-		System.out.println("Namespace:      " + appDetails.getNamespace());
-		System.out.println("Contact Email:  " + appDetails.getContactEmail());
-		System.out.println("Website URL:    " + appDetails.getWebsiteUrl());
-	}
+    public static void main(String[] args) {
+        String appId = promptForInput("Enter your App ID:");
+        String appSecret = promptForInput("Enter your App Secret:");
+        String appToken = fetchApplicationAccessToken(appId, appSecret);
+        AppDetails appDetails = fetchApplicationData(appId, appToken);
+        System.out.println("\n   APPLICATION DETAILS");
+        System.out.println("=========================");
+        System.out.println("ID:             " + appDetails.getId());
+        System.out.println("Name:           " + appDetails.getName());
+        System.out.println("Namespace:      " + appDetails.getNamespace());
+        System.out.println("Contact Email:  " + appDetails.getContactEmail());
+        System.out.println("Website URL:    " + appDetails.getWebsiteUrl());
+    }
 
-	@SuppressWarnings("unchecked")
-	private static AppDetails fetchApplicationData(String appId, String appToken) {
-		Facebook facebook = new FacebookTemplate(appToken);
-		return facebook.restOperations().getForObject("https://graph.facebook.com/{appId}?fields=name,namespace,contact_email,website_url", AppDetails.class, appId);
-	}
+    @SuppressWarnings("unchecked")
+    private static AppDetails fetchApplicationData(String appId, String appToken) {
+        Facebook facebook = new FacebookTemplate(appToken);
+        return facebook.restOperations().getForObject("https://graph.facebook.com/{appId}?fields=name,namespace,contact_email,website_url", AppDetails.class, appId);
+    }
 
-	private static String fetchApplicationAccessToken(String appId, String appSecret) {
-		OAuth2Operations oauth = new FacebookConnectionFactory(appId, appSecret).getOAuthOperations();
-		return oauth.authenticateClient().getAccessToken();
-	}
-	
-	private static String promptForInput(String promptText) {
-		return JOptionPane.showInputDialog(promptText + " ");
-//		try {
-//			System.out.print(promptText + " ");
-//			BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-//			return reader.readLine();
-//		} catch (IOException e) {
-//			System.err.println("IOException prompting user: " + e);
-//			System.exit(1);
-//		}
-//		return null;
-	}
-	
-	private static final class AppDetails {
-		private long id;
+    private static String fetchApplicationAccessToken(String appId, String appSecret) {
+        OAuth2Operations oauth = new FacebookConnectionFactory(appId, appSecret).getOAuthOperations();
+        return oauth.authenticateClient().getAccessToken();
+    }
+    
+    private static String promptForInput(String promptText) {
+        return JOptionPane.showInputDialog(promptText + " ");
+    }
+    
+    private static final class AppDetails {
+        private long id;
 
-		private String name;
-		
-		private String namespace;
-		
-		@JsonProperty("contact_email")
-		private String contactEmail;
-		
-		@JsonProperty("website_url")
-		private String websiteUrl;
-		
-		public long getId() {
-			return id;
-		}
-		public String getName() {
-			return name;
-		}
-		public String getNamespace() {
-			return namespace;
-		}
-		public String getContactEmail() {
-			return contactEmail;
-		}
-		public String getWebsiteUrl() {
-			return websiteUrl;
-		}
-	}
+        private String name;
+        
+        private String namespace;
+        
+        @JsonProperty("contact_email")
+        private String contactEmail;
+        
+        @JsonProperty("website_url")
+        private String websiteUrl;
+        
+        public long getId() {
+            return id;
+        }
+        public String getName() {
+            return name;
+        }
+        public String getNamespace() {
+            return namespace;
+        }
+        public String getContactEmail() {
+            return contactEmail;
+        }
+        public String getWebsiteUrl() {
+            return websiteUrl;
+        }
+    }
 
 }
